c054222cc8ca80c5972a9db09b43f4e41bf9a683
Jason Arnold <jason.arnold.00@gmail.com>
2018-05-30 16:46:21 -0400

    add comments

--- js/app.js
+++ js/app.js
@@ -49,44 +49,60 @@ class Player {
   }
 
   update() {
+    // If the player is in the same row as an enemy start detecting for
+    // collisions
     if (this.y === 230 || this.y === 145 || this.y === 60) {
       this.detectCollisions(this.x, this.y)
+    // if the player makes it to the last row, they win
     } else if (this.y === -25) {
       this.playerWins();
     }
   }
 
   detectCollisions(playerX, playerY) {
+    // Compare players x value with the enemies x values, if the difference
+    // between them reaches less than 60 pixels, then a collision has occurred
+    // find array helper - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/find
     let enemyInCurrentLane = allEnemies.find(enemy => enemy.y === playerY);
     if (playerX - enemyInCurrentLane.x <= 60 && playerX - enemyInCurrentLane.x >= 0) {
+      // Deduct one from current lives and update DOM
       this.lives -= 1;
       livesText.textContent = this.lives;
+      // If lives reaches 0 then the game is over
       if (this.lives === 0) {
         this.resetPlayer();
         gameOver();
       } else {
+      // If lives remain, put player back to start position
         this.resetPlayer();
       }
     }
   }
 
   playerWins() {
+    // Increase current score by 1, update DOM, and put player back at start
     this.score += 1;
     scoreText.textContent = this.score;
     this.resetPlayer();
   }
 
   resetPlayer() {
+    // Put player back at start position
     this.x = 200;
     this.y = 400;
   }
 
   changeSprite(charImg) {
+    // Get path for the player's current sprite
     let currentSpritePath = player.sprite
+    // The path for the new sprite
     let newSpritePath = charImg.attributes.src.nodeValue
     let allImgItems = document.querySelectorAll('li');
+    // Loop through all of the sprite images and remove the 'chosen' CSS class
     allImgItems.forEach(img => img.classList.remove('chosen'));
+    // Change the player's sprite to the new one
     player.sprite = newSpritePath;
+    // Add the 'chosen' CSS class to the new sprite
     charImg.parentElement.classList.add('chosen');
   }
 
@@ -127,6 +143,7 @@ const playAgainBtn = document.querySelector('.play-again-btn');
 const charList = document.querySelector('.char-list');
 
 resetGame = () => {
+  // Resets the game back to start settings
   player.lives = 3;
   player.score = 0;
   player.resetPlayer();
@@ -134,11 +151,13 @@ resetGame = () => {
 }
 
 resetStats = () => {
+  // Resets the number of lives and the score
   scoreText.innerText = 0;
   livesText.innerText = 3;
 }
 
 gameOver = () => {
+  // When the game is over show the modal including the final score
   const finalScore = document.querySelector('.num-score-final');
   modal.classList.toggle('hidden');
   modal.classList.toggle('bounceIn');
@@ -158,12 +177,14 @@ document.addEventListener('keyup', function(e) {
     player.handleInput(allowedKeys[e.keyCode]);
 });
 
+// Event listener for 'Play Again?' button
 playAgainBtn.addEventListener('click', function(e) {
   modal.classList.toggle('bounceIn');
   modal.classList.toggle('hidden');
   resetGame();
 })
 
+// Event listener to select a new player sprite
 charList.addEventListener('click', function(e) {
   if (e.target.nodeName === 'IMG') {
     player.changeSprite(e.target);
